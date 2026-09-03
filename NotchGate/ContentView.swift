fdc382468de06b1400bb6923b3f3a29672c534e8
Naheem Innis <n.k.innis92@gmail.com>
2026-09-02 23:51:57 -0400

    Initial Commit

---
+++ NotchGate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotchGate
+//
+//  Created by Naheem Innis on 9/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotchGate/NotchGateApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotchGateApp.swift
+//  NotchGate
+//
+//  Created by Naheem Innis on 9/2/26.
+//
+
+import SwiftUI
+
+@main
+struct NotchGateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
